13b28073177067f5cf8d5b18b3130cf5db7877a6
AlexKiryushkin <sashakir94@mail.ru>
2021-01-21 10:23:15 +0700

    Removed excessive whitespaces

--- include/rttg/impl/visit.h
+++ include/rttg/impl/visit.h
@@ -8,17 +8,17 @@
 namespace rttg
 {
 
-    /**
-     * A thin wrapper around std::visit function.
-     * Calls callable with real references instead of std::reference_wrapper.
-     */
-    template <class CallableT, class... VariantTs,
-              class = std::enable_if_t<(true && ... && IsVariantRefV<VariantTs>)>>
-    auto visit(CallableT && callable, VariantTs && ... variants)
-    {
-        return std::visit(
-            [&callable](auto && value) { return std::invoke(std::forward<CallableT>(callable), value.get()); },
-            std::forward<VariantTs>(variants)...);
-    }
+/**
+ * A thin wrapper around std::visit function.
+ * Calls callable with real references instead of std::reference_wrapper.
+ */
+template <class CallableT, class... VariantTs,
+          class = std::enable_if_t<(true && ... && IsVariantRefV<VariantTs>)>>
+auto visit(CallableT && callable, VariantTs && ... variants)
+{
+    return std::visit(
+        [&callable](auto && value) { return std::invoke(std::forward<CallableT>(callable), value.get()); },
+        std::forward<VariantTs>(variants)...);
+}
 
 } // namespace rttg
